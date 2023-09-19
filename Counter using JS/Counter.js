1f5ff20650e34a4c5797f50017149a2999171df6
Mayar <mayardarwish21@gmail.com>
2023-09-19 14:50:39 +0300

    counter.js

---
+++ Counter using JS/Counter.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+counter = 0;
+var createCounter = function (n) {
+
+  counter = n;
+  return function () {
+    return counter++;
+
+  };
+
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
